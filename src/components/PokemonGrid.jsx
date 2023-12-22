c7931ce1dc703dc345a66dbb568ca2114c4b4c3f
Alex <jcotamomoli@uniminuto.edu.co>
2023-12-21 23:10:05 -0500

    se modifica

--- src/components/PokemonGrid.jsx
+++ src/components/PokemonGrid.jsx
@@ -1,22 +1,40 @@
-
-
-export const PokemonGrid = ({category}) => {
+const getPokemon = async (category) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${category}`;
+    const resp = await fetch(url);
+    const data = await resp.json();
   
-const getPokemon= async()=>{
-    const url= `https://pokeapi.co/api/v2/pokemon/${category}`
-    const resp= await fetch(url)
-    const {results.url} = await resp.json
-    console.log(resp)
-
+    // Verifica si la propiedad 'sprites' existe en la respuesta
+    if (data.sprites) {
+      const items = {
+        id: data.id,
+        name: data.name,
+        url: data.sprites.front_default,
+      };
   
-
-}
+      console.log(items);
+      return items;
+    } else {
+      console.error("Sprites no encontrados en la respuesta");
+      return null;
+    }
+  };
   
-getPokemon()
+  export const PokemonGrid = ({ category }) => {
+    const fetchData = async () => {
+      const pokemonData = await getPokemon(category);
+      if (pokemonData) {
+        // Haz algo con los datos, por ejemplo, guárdalos en el estado
+        console.log(pokemonData);
+      }
+    };
+  
+    fetchData();
+  
     return (
-   <>
-  <h3>{category}</h3>
-  <p>Hello</p> 
-   </>
-  )
-}
+      <>
+        <h3>{category}</h3>
+        {/* Renderiza los datos del Pokémon aquí */}
+      </>
+    );
+  };
+  
